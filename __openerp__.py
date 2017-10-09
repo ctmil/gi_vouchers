130ec5c64fa8cf96691c10ec05f644998c2c45af
morrillo <gustavo.orrillo@gmail.com>
2017-10-09 12:28:06 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'GI Vouchers',
+    'category': 'Accounting',
+    'version': '0.1',
+    'depends': ['base','account','sale','branches','l10n_ar_invoice','pagos_tarjeta_credito','l10n_ar_fpoc','account_voucher'],
+    'data': [
+	'account_report.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
