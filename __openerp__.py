7af08870553e5fa13bff09910e61a048f2e68133
morrillo <gustavo.orrillo@gmail.com>
2017-10-09 17:39:49 -0300

    Added checks

--- __openerp__.py
+++ __openerp__.py
@@ -2,7 +2,7 @@
     'name': 'GI Vouchers',
     'category': 'Accounting',
     'version': '0.1',
-    'depends': ['base','account','sale','branches','l10n_ar_invoice','pagos_tarjeta_credito','l10n_ar_fpoc','account_voucher'],
+    'depends': ['base','account','sale','branches','l10n_ar_invoice','pagos_tarjeta_credito','l10n_ar_fpoc','account_voucher','account_check'],
     'data': [
 	'account_report.xml',
     ],
